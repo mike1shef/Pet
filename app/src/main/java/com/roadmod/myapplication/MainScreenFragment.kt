6f118087146be84d9ed7fdf1433fda90bb5eab47
mikhailshauchenka <mike1shef@gmail.com>
2023-03-10 09:29:53 +0100

    Initial commit

---
+++ app/src/main/java/com/roadmod/myapplication/MainScreenFragment.kt
@@ -0,0 +1,23 @@
+ package com.roadmod.myapplication
+ import android.os.Bundle
+ import androidx.fragment.app.Fragment
+ import android.view.LayoutInflater
+ import android.view.View
+ import android.view.ViewGroup
+
+ class MainScreenFragment : Fragment() {
+
+        companion object {
+            fun newInstance() = MementoFragment()
+        }
+
+        private lateinit var viewModel: MementoViewModel
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return inflater.inflate(R.layout.fragment_main_screen, container, false)
+        }
+
+ }
---
+++ app/src/main/java/com/roadmod/myapplication/MementoViewModel.kt
@@ -0,0 +1,7 @@
+package com.roadmod.myapplication
+
+import androidx.lifecycle.ViewModel
+
+class MementoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
